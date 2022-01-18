0a8c6cec3988957533ab7b06f1722353c98e4491
Neat-Hyper-txt <viditgupta70000@gmail.com>
2022-01-19 03:55:53 +0530

    Last Commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({extended:true}))
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 const Product = require('./models/product')
 
 const mongoose = require('mongoose');
@@ -16,12 +20,27 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
         console.log(err);
     })
 
+
+
+
+const categories = ['fruits','vegetables','dairy'];
+
 //routes
 
 app.get('/products',async (req,res)=>
 {
     const products = await Product.find({})
     res.render('products/index',{products});
+})
+app.get('/products/new',(req,res)=>
+{
+    res.render('products/new',{categories});
+})
+app.post('/products',async (req,res)=>
+{
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
 })
 
 app.get('/products/:id',async (req,res)=>
@@ -30,16 +49,27 @@ app.get('/products/:id',async (req,res)=>
     const foundProduct = await Product.findById(id);
     res.render('products/show',{foundProduct});
 })
-app.get('/productsNew',(req,res)=>
+app.get('/products/:id/edit',async (req,res)=>
 {
-    res.render('products/new');
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit',{product,categories});
+})
+app.put('/products/:id',async (req,res)=>
+{
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id,req.body,{runValidators:true,new:true});
+    res.redirect(`/products/${product._id}`);
 })
 
-app.post('/products',(req,res)=>
-{
-    console.log(req.body);
-    res.send('Making Your Product');
-})
+
+
+
+
+
+
+
+
 
 // port
 
